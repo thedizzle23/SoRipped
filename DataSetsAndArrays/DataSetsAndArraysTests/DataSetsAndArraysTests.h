5e992e5ac3466b27f1b02392264bb112da5f2194
Robert DeNicola <thedizzle23@gmail.com>
2012-07-25 10:46:09 -0400

    online edit

---
+++ DataSetsAndArrays/DataSetsAndArraysTests/DataSetsAndArraysTests.h
@@ -0,0 +1,13 @@
+//
+//  DataSetsAndArraysTests.h
+//  DataSetsAndArraysTests
+//
+//  Created by Rob DeNicola on 7/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DataSetsAndArraysTests : SenTestCase
+
+@end
---
+++ DataSetsAndArrays/DataSetsAndArraysTests/DataSetsAndArraysTests.m
@@ -0,0 +1,32 @@
+//
+//  DataSetsAndArraysTests.m
+//  DataSetsAndArraysTests
+//
+//  Created by Rob DeNicola on 7/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DataSetsAndArraysTests.h"
+
+@implementation DataSetsAndArraysTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DataSetsAndArraysTests");
+}
+
+@end
